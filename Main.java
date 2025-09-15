8ffe8b2300655ec38785b0dd118f3efa93a0095b
Angela <angelayourant@nycstudents.net>
2025-09-15 12:54:28 -0400

    Update Main.java
    Final

--- Main.java
+++ Main.java
@@ -7,15 +7,15 @@ public class Main
         System.out.println("Current Food: " + f.getCurrentFood());
         f.simulateOneDay(12);
         System.out.println("Current Food: " + f.getCurrentFood());
-        f.simulateOneDay(22);
         f = new Feeder(1000);
+        f.simulateOneDay(22);
         System.out.println("Current Food: " + f.getCurrentFood());
         f = new Feeder(2400);
         System.out.println(f.simulateManyDays(10,4) + " days");
         f = new Feeder(250);
-        System.out.println(f.simulateMany Days (10, 5) + day(s)");
+        System.out.println(f.simulateManyDays (10, 5) + "day(s)");
         f = new Feeder(0);
-        System.out.println(f.simulateMany Days (5, 10) + day(s)");
+        System.out.println(f.simulateManyDays (5, 10) + "day(s)");
         // System.out.println(Math.random());
         // System.out.println(Math.random() * 10);
         // System.out.println((int) (Math.random() * 10)+1);
