e649a221919156aad4c58af74f4a038ec1ceb118
Angela <angelayourant@nycstudents.net>
2025-09-15 04:36:34 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder(500);
+        Feeder f = new Feeder(1000);
         System.out.println(f);
         System.out.println(f.getCurrentFood());
         System.out.println(Math.random());
         System.out.println(Math.random() * 10);
         System.out.println((int) (Math.random() * 10)+1);
-        f.simulateOneDay(12);
+        f.simulateOneDay(22);
         System.out.println(f.getCurrentFood());
     }
 }
