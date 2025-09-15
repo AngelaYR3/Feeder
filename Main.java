8b6b5fe583aab940248edb5b5502de1e50ec85c2
Angela <angelayourant@nycstudents.net>
2025-09-15 04:49:14 -0400

    Update Main.java
    Stimulating days

--- Main.java
+++ Main.java
@@ -2,14 +2,21 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder(1000);
+        Feeder f = new Feeder(500);
         System.out.println(f);
-        System.out.println(f.getCurrentFood());
-        System.out.println(Math.random());
-        System.out.println(Math.random() * 10);
-        System.out.println((int) (Math.random() * 10)+1);
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f.simulateOneDay(12);
+        System.out.println("Current Food: " + f.getCurrentFood());
         f.simulateOneDay(22);
-        System.out.println(f.getCurrentFood());
+        f = new Feeder(1000);
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f = new Feeder(2400);
+        System.out.println(f.simulateManyDays(10,4) + " days");
+        // System.out.println(Math.random());
+        // System.out.println(Math.random() * 10);
+        // System.out.println((int) (Math.random() * 10)+1);
+        // f.simulateOneDay(22);
+        // System.out.println(f.getCurrentFood());
     }
 }
 
